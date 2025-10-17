815dca0f7d6ff37175507f39f2add050a40137f8
PetrOstrovskiy <118372350+PetrOstrovskiy@users.noreply.github.com>
2025-10-17 23:09:02 +0700

    Проект без разделения на слои

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,23 +1,22 @@
 package service
 
-//
-//import (
-//	"aggregationSubscriptions/internal/models"
-//	"aggregationSubscriptions/internal/repository"
-//)
-//
-//type Service interface {
-//	GetAllSubscriptions() ([]*models.Subscription, error)
-//	GetSubscriptionByID(id string) (*models.Subscription, error)
-//	CreateNewSubscription(sub models.Subscription) error
-//	UpdateSubscriptionByID(id string, data models.Subscription) (*models.Subscription, error)
-//	DeleteSubscriptionByID(id string) error
-//	//GetCountSubscriptionsPrice(query) ([]*models.Subscription, error)
-//}
-//type service struct {
-//	repo repository.Repository
-//}
-//
-//func NewService(repo repository.Repository) Service {
-//	return &service{repo: repo}
-//}
+import (
+	"aggregationSubscriptions/internal/models"
+	"aggregationSubscriptions/internal/repository"
+)
+
+type Service interface {
+	GetAllSubscriptions() ([]*models.Subscription, error)
+	GetSubscriptionByID(id string) (*models.Subscription, error)
+	CreateNewSubscription(sub models.Subscription) error
+	UpdateSubscriptionByID(id string, data models.Subscription) (*models.Subscription, error)
+	DeleteSubscriptionByID(id string) error
+	//GetCountSubscriptionsPrice(query) ([]*models.Subscription, error)
+}
+type service struct {
+	repo repository.Repository
+}
+
+func NewService(repo repository.Repository) Service {
+	return &service{repo: repo}
+}
